0131da5b12fda027c6b98ea9be963b3ba512bfc5
Vk4011 <maticsystem1@gmail.com>
2024-03-04 01:03:45 +0530

    CryptoContext

---
+++ src/components/Main.jsx
@@ -0,0 +1,16 @@
+import React  from "react";
+import TableComponent from "./TableComponent";
+
+const Main=()=>{
+    return (
+        <>
+        <section 
+        className="w-[80%] h-full flex flex-col mt-16 mb-24 relative"
+        >
+         <TableComponent/>
+        </section>
+        </>
+    )
+}
+
+export default Main
